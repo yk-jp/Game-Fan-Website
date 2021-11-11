3417e3935ecd41705b36c65ca7c620bf59181c3b
yusuke <ID+username@users.noreply.github.com>
2021-11-11 23:16:49 +0900

    added setup for bootstrap

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
     path('', views.index, name='landingPage'),
     path('rules', views.rules, name='ruleExplained'),
-    path('notables', views.notablesDetail, name='notablesDetail'),
-    path('notables', views.notablesList, name='notablesList'),
-    path('externalLinks', views.externalLinks, name='externalLinks'),
+    path('notables/<int:notablesIndex>/', views.notablesDetail, name='notablesDetail'),
+    path('notables/', views.notablesList, name='notablesList'),
+    path('externalLinks/', views.externalLinks, name='externalLinks'),
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -1,16 +1,17 @@
 from django.shortcuts import render
+from django.template import loader
 from django.http import HttpResponse
 
 # Create your views here.
 # landing page
 def index(request):
-    return HttpResponse("Hello, world. You're at the profiles index.")
+    return render(request, 'pages/landingPage/index.html')
 
 def rules(request):
      return HttpResponse("rule page")
 
-def notablesDetail(request):
-     return HttpResponse("notablesDetail")
+def notablesDetail(request,notablesIndex):
+     return HttpResponse("notablesDetail %s" %notablesIndex)
     
 def notablesList(request):
     return HttpResponse("notablesList")
