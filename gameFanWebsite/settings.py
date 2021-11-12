dbb529dedce87a7e1eac40b9808c9c9002ea7cbb
yusuke <ID+username@users.noreply.github.com>
2021-11-12 17:47:08 +0900

    created the landing page

--- gameFanWebsite/settings.py
+++ gameFanWebsite/settings.py
@@ -125,7 +125,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static"
+    BASE_DIR / "static",
+    BASE_DIR / "profiles" / "assets"
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
